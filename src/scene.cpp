1a77f26837b780291b9036cdb07feee54afb0213
grehgn <gregoire.hugon@ensta-paris.fr>
2022-03-15 17:52:16 +0100

    slope from derivative

--- src/scene.cpp
+++ src/scene.cpp
@@ -11,7 +11,7 @@ void scene_structure::initialize()
 {
 	// Initialize the shapes of the scene
 	// ***************************************** //
-	int N = 50;
+	N = 50;
 	int Lfactor = 20;
 	int Wfactor = 20;
 	// Set the behavior of the camera and its initial position
@@ -34,7 +34,7 @@ void scene_structure::initialize()
 	}
 	floor = mesh_primitive_grid({ 0,0,0 }, { 1,0,0 }, { 1,1,0 }, { 0,1,0 }, N, Lfactor* N);
 	for (int i = 0; i < N * N * Lfactor; i++){
-		floor.position.at(i)=vec3(Wfactor * floor.position.at(i).x, Lfactor * floor.position.at(i).y,  atan_floor(floor.position.at(i).y));//- 3.2);
+		floor.position.at(i)=vec3(Wfactor * floor.position.at(i).x, Lfactor * floor.position.at(i).y,  atan_floor(floor.position.at(i).y)-floor_offset);//- 3.2);
 	}
 	environment.camera.look_at({ 5.0f,-4.0f,2.0f }, shape.position.at(N * N * Lfactor / 2 - N/2 ));
 	//shape.position.at(N/2, 5*N/2, n/2)
@@ -109,7 +109,7 @@ void scene_structure::display_gui()
 
 void scene_structure::evolve_shape()
 {
-    size_t const N = initial_position.size();
+    size_t const M = initial_position.size();
 
 	//wind
 	vec3 wind_dir = normalize(vec3(std::cos(wind_angle), std::sin(wind_angle), 0.0));
@@ -123,24 +123,27 @@ void scene_structure::evolve_shape()
 	vec3 K_vec = K * wind_dir;
 	float lambda = 1.5;
 
-	float K_o = 30.0;
-	float K_y = 30.0;
-	float K_z = 30.0;
+	float K_y = 1.0;
+	float K_z = 1.0;
 	//std::cout << "R = " << R << " w = " << w << " K = " << K <<"\n";
-    for(size_t k=0; k<N; ++k)
+    for(size_t k=0; k<M; ++k)
     {
-        
 		vec3 const& p0 = initial_position[k];
         vec3& p        = shape.position[k];
-		float depth =  -1*sloped_floor( p0.y, 02.0, 0.23)+floor_offset;
+		float gamma = 0;
+		if (k>N && k < M-N) gamma = (floor.position[k+N].z - floor.position[k-N].z)/(floor.position[k+N].y - floor.position[k-N].y);
+		else if (k > M-N-1) gamma = (floor.position[M-1].z - floor.position[M-1-N].z)/(floor.position[M-1].y - floor.position[M-1-N].y);
+		float depth =  -1*floor.position[k].z;
 		float K_depth = K / std::sqrt( std::tanh(K * depth));
 		float w_depth = 0;
 
 		float phi = - w * timer.t + K_depth * dot(wind_dir, p0) ;//K_integration(K ,dot(wind_dir, p0), &valley_floor);
 		//float alpha = -1 * K_depth * dot(wind_dir_2, p0);
 		float alpha = 0.23 * std::exp(-0.1 * K_depth * depth ) ;
-		float Sx = 1 / (1 - std::exp(-1 * K_depth * depth));
-		float Sz = Sx * (1 - std::exp(-1 * K_depth * depth));
+		float Sx = 1 / (1 - std::exp(-1 * K_y * depth));
+		float Sz = Sx * (1 - std::exp(-1 * K_z * depth));
+
+		float alpha2 = std::asin(sin(gamma)*std::exp(-K_depth*depth));
 		
         //p.x = p0.x + R * dot(wind_dir, vec3(1,0,0)) * std::sin(K * p0.y - w * timer.t - lambda * (p.z - p0.z) * (timer.t - initial_time));
 		//p.y = p0.y + R  * std::sin( K_depth * dot(wind_dir, p0) - w * timer.t - lambda * (p.z - p0.z) * (timer.t - initial_time));
@@ -150,10 +153,10 @@ void scene_structure::evolve_shape()
 		//p.y= p0.y + 2*R * std::cos(phi + alpha - lambda * (p.z - p0.z) * (timer.t - initial_time));
 		//p.z= p0.z +3*R * std::sin(phi + alpha - lambda * (p.z - p0.z) * (timer.t - initial_time));
 
-		p.y = p0.y + R * std::cos(alpha) * Sx * std::sin(phi) +
-			 std::sin(alpha) * Sz * std::cos(phi);
-		p.z = p0.z + R *  std::cos(alpha) * Sz * std::sin(phi) +
-			 std::sin(alpha) * Sx * std::cos(phi);
+		p.y = p0.y + R * std::cos(alpha2) * Sx * std::sin(phi) +
+			 std::sin(alpha2) * Sz * std::cos(phi);
+		p.z = p0.z + R *  std::cos(alpha2) * Sz * std::sin(phi) +
+			 std::sin(alpha2) * Sx * std::cos(phi);
 			
     }
 }
@@ -177,7 +180,7 @@ float sloped_floor(float x, float limit, float slope){
 }
 
 float atan_floor(float x){
-	return 3*(std::atan(0.5*(x-10))-1.5);
+	return 0.5*(std::atan(40*(x-0.4))-1.7);
 }
 
 float valley_floor(float x, float position, float width){
--- src/scene.hpp
+++ src/scene.hpp
@@ -39,9 +39,6 @@ struct scene_structure {
 	float initial_time;
 	cgp::mesh_drawable shape_visual;
 	cgp::mesh_drawable floor_visual;
-	float wind_str = 2.0;
-	float wind_angle = 1.6;
-	float floor_offset = 5.0;
 
 	cgp::timer_basic timer; // A timer to have access to the elapsed time
 	cgp::scene_environment_basic_camera_spherical_coords environment; // Standard environment controler
@@ -57,6 +54,11 @@ struct scene_structure {
 
 	
 	void evolve_shape();
+	
+	float wind_str = 2.0;
+	float wind_angle = 1.6;
+	float floor_offset = 5.0;
+	int N = 50;
 
 };
 float K_integration(float K ,float x0, float (*h)(float, float, float));
